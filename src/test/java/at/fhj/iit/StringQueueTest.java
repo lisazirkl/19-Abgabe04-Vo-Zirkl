dd4314481c411716fb91bdf12c4ff370ca335631
Cong Thanh Nhan Vo <cong.vo@edu.campus02.at>
2019-06-04 23:42:42 +0200

    configured Readme and added Comments in TestStringQueue

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -11,6 +11,11 @@ public class StringQueueTest {
     private StringQueue queueOne;
     private StringQueue queueTwo;
 
+
+    /**
+     * setUp for testing
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception
     {
@@ -18,6 +23,10 @@ public class StringQueueTest {
         queueTwo = new StringQueue(0);
     }
 
+    /**
+     * First test for offer method
+     * @result must be true
+     */
     @Test
     public void testOffer()
     {
@@ -26,11 +35,20 @@ public class StringQueueTest {
         queueOne.offer("test3");
         assertTrue(queueOne.offer("test4"));
     }
+
+    /**
+     * Second test for offer method
+     * @result must be false
+     */
     @Test
     public void testOffer2(){
         assertFalse(queueTwo.offer("blaaaaa"));
     }
 
+    /**
+     * test poll method adding 3 Strings
+     * @result must be the first added String
+     */
     @Test
     public void testPoll(){
         queueOne.offer("test");
@@ -40,6 +58,11 @@ public class StringQueueTest {
 
     }
 
+    /**
+     * test remove method
+     *remove the first added String
+     * @result must be the removed String
+     */
     @Test
     public void testRemove(){
         queueOne.offer("test");
@@ -48,11 +71,17 @@ public class StringQueueTest {
         assertEquals("test",queueOne.poll());
     }
 
+    /**
+     * test Exception
+     */
     @Test (expected = NoSuchElementException.class)
     public void testRemove2(){
         queueOne.remove();
     }
 
+    /**
+     * test peek method
+     */
     @Test
     public void testPeek()
     {
@@ -61,6 +90,26 @@ public class StringQueueTest {
         assertEquals("test", queueOne.peek());
         queueOne.remove();
         assertEquals("test2", queueOne.peek());
+    }
+
+    /**
+     * test Exception
+     */
+    @Test (expected = NoSuchElementException.class)
+    public void testElement(){
+        queueOne.offer("test");
+        queueOne.remove();
+        queueOne.element();
+    }
+
+    /**
+     * test element
+     */
+    @Test
+    public void testElement2(){
+        queueOne.offer("hallo");
+        queueOne.offer("ich");
+        assertEquals("hallo",queueOne.element());
     }
 
 
